78d1098361164de93ec45cf090ce0e6682cb0c18
johnhaq69 <yojohnn04@gmail.com>
2022-12-01 10:04:05 +0700

    projek awal

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RiceMillKu',
+      title: 'RiceMillKu Makmur Jaya',
       debugShowCheckedModeBanner: false,
       routes: {
         '/': (context) => LoginScreen(),
