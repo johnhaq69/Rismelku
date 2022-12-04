17fa01df80a5829d95cdb73a048868c4b525aa75
johnhaq69 <yojohnn04@gmail.com>
2022-12-04 22:08:56 +0700

    edit tambah_transaksi

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RiceMillKu Makmur Jaya',
+      title: 'RiceMillKu Hj Wahyah',
       debugShowCheckedModeBanner: false,
       routes: {
         '/': (context) => LoginScreen(),
--- lib/pages/tambah_transaksi.dart
+++ lib/pages/tambah_transaksi.dart
@@ -2,12 +2,27 @@ import 'package:rismelku/widget/bottom_nav.dart';
 import 'package:flutter/material.dart';
 import 'package:rismelku/theme.dart';
 
-class TambahTransaksiScreen extends StatelessWidget {
+class TambahTransaksiScreen extends StatefulWidget {
   const TambahTransaksiScreen({super.key});
+
+  @override
+  State<TambahTransaksiScreen> createState() => _TambahTransaksiScreenState();
+}
+
+class _TambahTransaksiScreenState extends State<TambahTransaksiScreen> {
+  List<String> listBarang = [
+    'Beras',
+    'Bekatul',
+    'Gabah',
+  ];
+  String? barang;
+
+  TextEditingController beratBarangController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: primaryColor,
         centerTitle: true,
@@ -22,14 +37,57 @@ class TambahTransaksiScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Text(
-          "Tambah Transaksi Screen",
-          style: TextStyle(
-            fontFamily: 'OkineSans',
-            fontSize: 25,
-            color: blackColor,
-          ),
+      body: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: <Widget>[
+                Text(
+                  "Jenis Barang : ",
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+              ],
+            ),
+            DropdownButtonFormField(
+              onChanged: (String? value) {
+                setState(() {
+                  barang = value;
+                });
+              },
+              value: barang,
+              items: listBarang.map((String value) {
+                return DropdownMenuItem(
+                  child: Text(value),
+                  value: value,
+                );
+              }).toList(),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  "Berat Barang : ",
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+              ],
+            ),
+            TextFormField(
+              controller: beratBarangController,
+              decoration: InputDecoration(
+                labelText: 'Berat Barang',
+                hintText: 'Berat Kg',
+                border: UnderlineInputBorder(),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: BottomNavWidget(),
